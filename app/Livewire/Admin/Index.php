b6fde13a2817b33df8759e3d22bbe3471b3e92e9
Saumy Anand <samcool3203@gmail.com>
2025-04-12 09:58:14 +0530

    pagination added in doctor

--- app/Livewire/Admin/Index.php
+++ app/Livewire/Admin/Index.php
@@ -2,14 +2,39 @@
 
 namespace App\Livewire\Admin;
 
+use App\Models\Appointment;
+use App\Models\Doctor;
+use App\Models\Patient;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class Index extends Component
 {
+    public $Appointments=0;
+    public $Patients = 0;
+    public $Doctors = 0;
+    public $Revenue = 0;
+    public function mount()
+    {
+        $this->Appointments = Appointment::count();
+        $this->Patients = Patient::count();
+        $this->Doctors = Doctor::count();
+        $this->Revenue = Appointment::with('doctor')
+        ->get()
+        ->sum(function ($appointment) {
+            return $appointment->doctor->fee ?? 0;
+        });   
+     }
     #[Layout('components.layouts.admin')]
     public function render()
     {
-        return view('livewire.admin.index');
+        $allAppointments = Appointment::get();
+        return view('livewire.admin.index', [
+            'Appointments' => $this->Appointments,
+            'Patients' => $this->Patients,
+            'Doctors' => $this->Doctors,
+            'Revenue' => $this->Revenue,
+            'allAppointments' => $allAppointments,  
+        ]);
     }
 }
