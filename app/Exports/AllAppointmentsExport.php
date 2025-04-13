245f063d58d406c1d0d3a5278cdd79208ac52478
Saumy Anand <samcool3203@gmail.com>
2025-04-13 12:39:47 +0530

    import header issue fixed

--- app/Exports/AllAppointmentsExport.php
+++ app/Exports/AllAppointmentsExport.php
@@ -3,8 +3,9 @@
 namespace App\Exports;
 
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class AllAppointmentsExport implements FromCollection
+class AllAppointmentsExport implements FromCollection, WithHeadings
 {
     protected $appointments;
 
@@ -17,14 +18,27 @@ class AllAppointmentsExport implements FromCollection
     {
         return $this->appointments->map(function ($appointment) {
             return [
-                'patient_id' => $appointment->patient_id,
-                'appointment_id' => $appointment->id,
-                'Doctor'    => $appointment->doctor->user->name ?? 'N/A',
-                'Patient'   => $appointment->patient->name ?? 'N/A',
-                'Date'      => \Carbon\Carbon::parse($appointment->appointment_date)->format('d M Y'),
-                'Time'      => \Carbon\Carbon::parse($appointment->time)->format('h:i A'),
-                'Status'    => ucfirst(str_replace('_', ' ', $appointment->status)),
+                'patient_id'      => $appointment->patient_id,
+                'appointment_id'  => $appointment->id,
+                'Doctor'          => $appointment->doctor->user->name ?? 'N/A',
+                'Patient'         => $appointment->patient->name ?? 'N/A',
+                'Date'            => \Carbon\Carbon::parse($appointment->appointment_date)->format('d M Y'),
+                'Time'            => \Carbon\Carbon::parse($appointment->time)->format('h:i A'),
+                'Status'          => ucfirst(str_replace('_', ' ', $appointment->status)),
             ];
         });
+    }
+
+    public function headings(): array
+    {
+        return [
+            'Patient ID',
+            'Appointment ID',
+            'Doctor',
+            'Patient',
+            'Date',
+            'Time',
+            'Status',
+        ];
     }
 }
