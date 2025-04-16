699ede6cd96e1f97d696ffbfa8783df5d181b1a6
smi-riti <smriti.kumari5104@gmail.com>
2025-04-16 13:16:23 +0530

    Implement career management feature with CRUD operations and job detail view

--- app/Livewire/PatientBooking/MenuItems/Careers.php
+++ app/Livewire/PatientBooking/MenuItems/Careers.php
@@ -4,15 +4,31 @@ namespace App\Livewire\PatientBooking\MenuItems;
 
 use Livewire\Attributes\Layout;
 use Livewire\Component;
+use App\Models\Career;
 
 class Careers extends Component
 {
     public $search = '';
-
+    public $showModal = false;
     #[Layout('layouts.guest')]
-    public function render()
+
+    public function openModal()
     {
-        // $jobs = Job::where('title', 'like', '%' . $this->search . '%')->latest()->get();
-        return view('livewire.patient-booking.menu-items.careers');
+        $this->showModal = true;
+    }
+
+    public function closeModal()
+    {
+        $this->showModal = false;
+    }
+    public function render()
+    {   
+        $jobs = Career::where('status', true)
+                      ->where('title', 'like', '%' . $this->search . '%')
+                      ->latest()
+                      ->get();
+        return view('livewire.patient-booking.menu-items.careers', [
+            'jobs' => $jobs
+        ]);
     }
 }
