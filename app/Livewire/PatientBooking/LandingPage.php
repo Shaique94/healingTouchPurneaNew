ec2ad45afd1b7fe416e3b856ec15c02136615ca6
smi-riti <smriti.kumari5104@gmail.com>
2025-04-23 17:54:54 +0530

    update default settings and enhance appointment management UI

--- app/Livewire/PatientBooking/LandingPage.php
+++ app/Livewire/PatientBooking/LandingPage.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\PatientBooking;
 
+use App\Models\Setting;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
@@ -10,6 +11,11 @@ class LandingPage extends Component
     #[Layout('layouts.guest')] 
     public function render()
     {
-        return view('livewire.patient-booking.landing-page');
+        return view('livewire.patient-booking.landing-page', [
+            'hospital_name' => Setting::get('hospital_name', 'Healing Touch Hospital'),
+            'contact_email' => Setting::get('contact_email', 'info@healingtouchpurnea.com'),
+            'contact_phone' => Setting::get('contact_phone', '+91 9471659700'),
+            'address' => Setting::get('address', 'Hope Chauraha, Rambagh Road, Linebazar, Purnea 854301'),
+        ]);
     }
 }
