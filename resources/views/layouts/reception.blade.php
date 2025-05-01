1d5503b13a0bc6e4a4c812704bb6cd4a41957feb
shaique <Shaique.9434@gmail.com>
2025-05-01 18:02:39 +0530

    fix: improve check-in confirmation and success alert messages

--- resources/views/layouts/reception.blade.php
+++ resources/views/layouts/reception.blade.php
@@ -120,31 +120,38 @@
                 )
             });
 
-      Livewire.on('confirm-check-in', ({ appointmentId }) => {
+      Livewire.on('confirm-check-in', (data) => {
+        const appointmentId = data[0].appointmentId;
+        // console.log('RECEIVED:', appointmentId); 
         Swal.fire({
             title: 'Are you sure?',
             text: "You are about to check-in this appointment!",
             icon: 'warning',
             showCancelButton: true,
             confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
+            cancelButtonColor: '#d33', 
             confirmButtonText: 'Yes, Check-in!',
             cancelButtonText: 'Cancel'
         }).then((result) => {
             if (result.isConfirmed) {
-                Livewire.dispatch('doCheckIn', { appointmentId: appointmentId });
-            }
+                Livewire.dispatch('doCheckIn', { appointmentId });
+                console.log('sending appointment after confirmation', appointmentId);
+              }
         });
     });
 
-    Livewire.on('alert', ({ type, message }) => {
-        Swal.fire({
-            icon: type,
-            text: message,
-            timer: 2000,
-            showConfirmButton: false,
-        });
+
+Livewire.on('alert', (data) => {
+    const { type, message } = data[0]; // access the first item in the array
+
+
+    Swal.fire({
+        icon: type,
+        text: message,
+        timer: 2000,
+        showConfirmButton: false,
     });
+});
     });
   </script>
 
