c26ec69b9b7921446b49834fc78c11167b610fcd
Saumy Anand <samcool3203@gmail.com>
2025-05-08 13:29:50 +0530

    remove svg

--- resources/views/livewire/reception/dashboard.blade.php
+++ resources/views/livewire/reception/dashboard.blade.php
@@ -250,11 +250,7 @@
                             <option value="female">Female</option>
                             <option value="other">Other</option>
                         </select>
-                        <div class="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
-                            <svg class="w-5 h-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
-                            </svg>
-                        </div>
+                    
                         @error('gender') <span class="text-red-500 text-xs mt-1">{{ $message }}</span> @enderror
                     </div>
                 </div>
